7c9c04477a85d4c7be8f5af84987ab1ca739a006
comahax <123654dagao@sina.cn>
2017-07-02 01:55:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y zmap
+sudo apt-get install -y python-pip
+sudo -H pip install -r ~/webzmap/requirements.txt -i https://pypi.mirrors.ustc.edu.cn/simple/
+sudo python ~/webzmap/manage.py migrate
+sudo python ~/webzmap/manage.py createsuperuser << EOF
+root
+apple@apple.com
+123qweasd
+123qweasd
+EOF
+sudo python ~/webzmap/manage.py runserver 0.0.0.0:8000 &
