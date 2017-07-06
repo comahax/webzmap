7759bb6aced79b66a88932a27c87bbe2910621db
comahax <123654dagao@sina.cn>
2017-07-06 10:17:50 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #sudo cp ~/webzmap/createsuperuser.py /usr/local/lib/python2.7/dist-packages/django/contrib/auth/management/commands/
-sudo cp ~/webzmap/sources.list /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y zmap
+#old
+sudo cp ~/webzmap/sources1.list /etc/apt/sources.list
+sudo apt-get update -y
 sudo apt-get install -y python-pip
 sudo -H pip install -r ~/webzmap/requirements.txt -i https://pypi.mirrors.ustc.edu.cn/simple/
+sudo apt-get install -y expect
+#new source
+sudo cp ~/webzmap/sources2.list /etc/apt/sources.list
+sudo apt-get update -y
+sudo apt-get install -y zmap
 sudo python ~/webzmap/manage.py migrate
 sudo python ~/webzmap/manage.py zmapd start
 #sudo python ~/webzmap/manage.py createsuperuser << EOF
@@ -13,6 +18,5 @@ sudo python ~/webzmap/manage.py zmapd start
 #123qweasd
 #123qweasd
 #EOF
-sudo apt-get install -y expect
 sudo expect ~/webzmap/user.sh
 sudo python ~/webzmap/manage.py runserver 0.0.0.0:8000 &
